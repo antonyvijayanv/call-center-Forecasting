b106e18bf93f9d368968bddb4392096129892ab3
Vijayan Antony Ganamuthu <vijayan.antonyganamu@maqta.ae>
2024-02-27 22:59:42 +0400

    code is updated and working

--- DeepAR.py
+++ DeepAR.py
@@ -1,51 +1,41 @@
-import json
-from datetime import datetime, timedelta
-
+import datetime
 import pandas as pd
+import json
+import numpy as np
 from gluonts.dataset.common import ListDataset
-from gluonts.evaluation import Evaluator
-from gluonts.evaluation import make_evaluation_predictions
-from gluonts.mx.distribution import StudentTOutput  # likelihood
+from gluonts.dataset.util import to_pandas
 from gluonts.mx.model.deepar import DeepAREstimator
-from gluonts.mx.trainer import Trainer
+from gluonts.mx.distribution import ZeroInflatedNegativeBinomialOutput, StudentTOutput  # likelihood
 from gluonts.mx.trainer.learning_rate_scheduler import LearningRateReduction
-from gluonts.mx.trainer.model_averaging import SelectNBestMean, ModelAveraging
+from gluonts.mx.trainer import Trainer
+from gluonts.mx.trainer.model_averaging import ModelAveraging, SelectNBestSoftmax, SelectNBestMean
 
-df = pd.read_excel('./Call-Center-Dataset.xlsx')
+from gluonts.evaluation import Evaluator
+from gluonts.evaluation.backtest import make_evaluation_predictions
 
-df["Date"] = pd.to_datetime(df["Date"])
+# import matplotlib.pyplot as plt
 
-df["Time"] = df["Time"] * 24
-df['Time'] = df['Time'].apply(lambda x: (datetime.min + timedelta(hours=x)).strftime('%H:%M:%S'))
-
-df["CallReceivedDateTime"] = pd.to_datetime(df['Date'].dt.strftime('%Y-%m-%d') + ' ' + df['Time'].astype(str))
-
-df['Date'] = pd.to_datetime(df['Date'], format='%d-%m-%Y')
-
-df["Answered"] = df['Answered (Y/N)'].apply(lambda y: 1 if y == 'Y' else 0)
-
-dfCopy = df[df['Answered'] == 1].groupby('Date').size().reset_index(name='sumofansweredcalls')
-
-dfCopy.rename(columns={'Date': 'callsReceivedDate'}, inplace=True)
-
-dfCopy["sumofansweredcalls"].astype(int)
-
-
-dfCopy.set_index('callsReceivedDate', drop=True, inplace=True)
-
-dfCopy.to_excel("factorized-data.xlsx")
-
+df = pd.read_excel("C:\\Users\\vijayan.antonyganamu\\Documents\\Bit\\DS-Projects\\Call-Center\\dfcopy.xlsx")
+df.columns = ["month", "noOfCalls"]
+df = df[:-1]
+df.month = pd.to_datetime(df.month)
+df.set_index("month", drop=True, inplace=True)
+# print(df.index[60])
 training_data = ListDataset(
-    [{"start": dfCopy.index[0], "target": dfCopy.sumofansweredcalls[:"2021-02-28T00:00:00.000000"], }],
+    [{"start": df.index[0], "target": df.noOfCalls[:"2022-12-01T00:00:00.000000"], }],
     freq="D"
 )
+
+entry = next(iter(training_data))
+train_series = to_pandas(entry)
 
 test_data = ListDataset(
-    [{"start": dfCopy.index[60], "target": dfCopy.sumofansweredcalls[:"2021-03-31T00:00:00.000000"]}],
+    [{"start": df.index[60], "target": df.noOfCalls[:"2023-12-01T00:00:00.000000"]}],
     freq="D"
 )
 
-
+entry = next(iter(test_data))
+test_series = to_pandas(entry)
 
 callbacks = [
     LearningRateReduction(objective="min",
@@ -70,7 +60,6 @@ estimator = DeepAREstimator(
 
 predictor = estimator.train(training_data)
 
-
 forecast_it, ts_it = make_evaluation_predictions(
     dataset=test_data,  # test dataset
     predictor=predictor,  # predictor
@@ -83,10 +72,9 @@ for item in forecasts:
     print(f"forecasts--{item}")
 ts_entry = tss[0]
 forecast_entry = forecasts[0]
-print(f"forecast_entry--{forecast_entry}")
+# print(f"forecast_entry--{forecast_entry}")
 
 evaluator = Evaluator()
 agg_metrics, item_metrics = evaluator(iter(tss), iter(forecasts), num_series=len(test_data))
 
 print(json.dumps(agg_metrics, indent=4))
-
